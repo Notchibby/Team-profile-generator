ca4fe8996149bb689ee508ad8f53d91957319a72
Chibuike Okoye~ <talk2chibyke@yahoo.com>
2022-10-04 11:27:52 +1100

    started commenting js

--- develop/index.js
+++ develop/index.js
@@ -1,30 +1,30 @@
 const inquirer = require('inquirer')
-const fs = require ('fs')
+const fs = require('fs')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 const answersarray = [];
 
-function managerQuestion(){
+function managerQuestion() {
     inquirer.prompt([
         {
             type: 'input',
             name: 'Name',
             message: 'What is the team managers name?',
         },
-    
+
         {
             type: 'input',
             name: 'Id',
             message: 'What is the team managers ID?',
         },
-    
+
         {
             type: 'input',
             name: 'Email',
             message: 'What is the team managers Email address?',
         },
-    
+
         {
             type: 'input',
             name: 'officeNumber',
@@ -32,136 +32,130 @@ function managerQuestion(){
         },
     ])
 
-    .then((answers) => {
-        console.log(answers);
-        const newManager = new Manager(answers.Name, answers.Id, answers.Email, answers.officeNumber)
-        answersarray.push(newManager)
-        menu()
+        .then((answers) => {
+            const newManager = new Manager(answers.Name, answers.Id, answers.Email, answers.officeNumber)
+            answersarray.push(newManager)
+            menu()
 
+        })
+
+
+}
+
+function menu() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'teamMembers',
+        message: 'choose your team member',
+        choices: ['Engineer', 'Intern', 'I dont want anymore team members']
     })
+        .then((answers) => {
+            if (answers.teamMembers === 'Engineer') {
+                engineer()
+            }
+            if (answers.teamMembers === 'Intern') {
+                intern()
+            }
+            if (answers.teamMembers === 'I dont want anymore team members') {
 
+                const htmlgenerator = generateHtml(answersarray)
 
-}
-
-function menu(){
-    inquirer.prompt(  {
-                type: 'list',
-                name: 'teamMembers',
-                message: 'choose your team member',
-                choices: ['Engineer', 'Intern', 'I dont want anymore team members']
-            })
-            .then((answers) => {
-                console.log(answers);
-                if(answers.teamMembers === 'Engineer'){
-                    engineer() 
-                }
-                if(answers.teamMembers === 'Intern'){
-                    intern()
-                }
-                if(answers.teamMembers === 'I dont want anymore team members'){
-                    // module.exports = answersarray;
-                    
-                    const htmlgenerator = generateHtml(answersarray)
-                    console.log(htmlgenerator)
-                    
-                    fs.writeFileSync('../develop/dist/dist.html', htmlgenerator, (err) =>
+                fs.writeFileSync('../develop/dist/dist.html', htmlgenerator, (err) =>
                     err ? console.log(err) : console.log('Successfully created index.html!'))
-                }
-            })
+            }
+        })
 
 }
 
-function engineer(){
+function engineer() {
     inquirer.prompt([
         {
-                    type: 'input',
-                    name: 'Name',
-                    message: 'What is the Engineers name?',
-            
-                },
-            
-                {
-                    type: 'input',
-                    name: 'Id',
-                    message: 'What is the Engineers ID?',
-                   
-                },
-            
-                {
-                    type: 'input',
-                    name: 'Email',
-                    message: 'What is the Engineers Email address?',
-                    
-                },
-                
-                {
-                   type: 'input',
-                   name: 'github',
-                   message: 'What is the Engineers github Username?',
-                  
-                },
-            
-            
+            type: 'input',
+            name: 'Name',
+            message: 'What is the Engineers name?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is the Engineers ID?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is the Engineers Email address?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the Engineers github Username?',
+
+        },
+
+
     ])
 
-    .then((answers) => {
-        console.log(answers)
-        const newEnginner = new Engineer(answers.Name, answers.Id, answers.Email, answers.github)
-        answersarray.push(newEnginner)
-        menu ()
-})
+        .then((answers) => {
+            const newEnginner = new Engineer(answers.Name, answers.Id, answers.Email, answers.github)
+            answersarray.push(newEnginner)
+            menu()
+        })
 
 }
 
-function intern(){
+function intern() {
     inquirer.prompt([
         {
-                    type: 'input',
-                    name: 'Name',
-                    message: 'What is the Interns name?',
-                   
-                },
-            
-                {
-                    type: 'input',
-                    name: 'Id',
-                    message: 'What is the Interns ID?',
-                   
-                },
-            
-                {
-                    type: 'input',
-                    name: 'Email',
-                    message: 'What is the Interns Email address?',
-                   
-                },
-            
-                {
-                    type: 'input',
-                    name: 'school',
-                    message: 'What is the Interns School?',
-                    
-                },
+            type: 'input',
+            name: 'Name',
+            message: 'What is the Interns name?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'What is the Interns ID?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is the Interns Email address?',
+
+        },
+
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the Interns School?',
+
+        },
 
     ])
 
-    .then((answers) => {
-        console.log(answers)
-        const newIntern = new Intern(answers.Name, answers.Id, answers.Email, answers.school)
-        answersarray.push(newIntern)
-        menu ()
-})
+        .then((answers) => {
+            const newIntern = new Intern(answers.Name, answers.Id, answers.Email, answers.school)
+            answersarray.push(newIntern)
+            menu()
+        })
 
-    
+
 }
 
 
 const generateHtml = (answersarray) => {
     const htmlcard = [];
-for(let i = 0;  i < answersarray.length ; i++){
-    
-    if (answersarray[i].getRole() === 'Manager'){
-        htmlcard.push(`<div class="cardtitle">
+    for (let i = 0; i < answersarray.length; i++) {
+
+        if (answersarray[i].getRole() === 'Manager') {
+            htmlcard.push(`<div class="cardtitle">
         <div class="cardheader">
             <h2>${answersarray[i].getName()}</h2>
             <p><i class="fa fa-mug-hot"> Manager</i></p>
@@ -179,10 +173,10 @@ for(let i = 0;  i < answersarray.length ; i++){
         </div>
     </div>`)
 
-    }
+        }
 
-    if (answersarray[i].getRole()=== 'Engineer'){
-        htmlcard.push(`
+        if (answersarray[i].getRole() === 'Engineer') {
+            htmlcard.push(`
         <div class="cardtitle">
             <div class="cardheader">
                 <h2>${answersarray[i].getName()}</h2>
@@ -203,10 +197,10 @@ for(let i = 0;  i < answersarray.length ; i++){
 `)
 
 
-    }
+        }
 
-    if (answersarray[i].getRole()=== 'Intern'){
-        htmlcard.push(`<div class="cardtitle">
+        if (answersarray[i].getRole() === 'Intern') {
+            htmlcard.push(`<div class="cardtitle">
         <div class="cardheader">
             <h2>${answersarray[i].getName()}</h2>
             <p><i class="fa fa-user-graduate"> Intern</i></p>
@@ -226,12 +220,12 @@ for(let i = 0;  i < answersarray.length ; i++){
     </div>
 `)
 
+        }
+
     }
 
-}
 
-
-return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 <html lang="en-us">
 
 <head>
@@ -257,7 +251,7 @@ return `<!DOCTYPE html>
 }
 
 
-
+//function to initialize app
 function init() {
     managerQuestion()
 }
